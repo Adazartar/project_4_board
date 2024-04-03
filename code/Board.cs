743e766e892ec0695d16738e8c905f0fcb37a583
mantelloa <adam.mantello@student.unimelb.edu.au>
2024-04-03 15:27:58 +1100

    add hovering and selecting cells

--- code/Board.cs
+++ code/Board.cs
@@ -10,26 +10,28 @@ public sealed class Board : Component
 	[Property] float board_z = 1;
 	[Property] float cell_z = 2;
 
+	GameObject active_cell = null;
+	GameObject selected_cell = null;
+
 	Dictionary<(int, int), GameObject> cells = new Dictionary<(int, int), GameObject>();
-	protected override void OnStart(){
-		Transform.Position = new Vector3(Transform.Position.x, Transform.Position.y, board_z);
-		back_piece.Clone(new Vector3(0,0,0));
+	protected override void OnStart()
+	{
 		CreateBoard();
 	}
+	
 	protected override void OnUpdate()
 	{
-		var ray = Scene.Trace.Ray((Scene.Camera.ScreenPixelToRay(Mouse.Position)), 1000f).WithTag("cell").HitTriggersOnly().Run();
-		if(ray.Hit && ray.GameObject.Tags.Has("cell") && cells.ContainsValue(ray.GameObject)){			
-			Log.Info("hit cell");
-		}
+		HoverCells();
+		SelectCells();
 	}
 
 	public void CreateBoard(){
+		back_piece.Clone(new Vector3(0,0,board_z));
 		Vector3 starting_point = new Vector3(-(gap*(columns-1))/2, -(gap * (rows-1))/2, cell_z);
 		for(int i = 0; i < rows; i++){
 			for(int j = 0; j < columns; j++){
 				GameObject new_cell = cell_piece.Clone(new Vector3(starting_point.x + j * gap, starting_point.y + i * gap, starting_point.z));
-				new_cell.Components.Get<Cell>().setID(i, j);
+				new_cell.Components.Get<Cell>().SetID(i, j);
 				cells[(i, j)] = new_cell;
 			}
 		}
@@ -47,5 +49,37 @@ public sealed class Board : Component
 		green.g = 1;
 		green.a = 1;
 		cells[(row, column)].Components.Get<ModelRenderer>().Tint = green;
+	}
+
+	public void HoverCells(){
+		var ray = Scene.Trace.Ray((Scene.Camera.ScreenPixelToRay(Mouse.Position)), 1000f).WithAnyTags(["cell","back"]).HitTriggersOnly().Run();
+		if(ray.Hit && ray.GameObject.Tags.Has("cell") && ray.GameObject != active_cell){
+			if(active_cell == null) active_cell = ray.GameObject;		
+			active_cell.Components.Get<Cell>().Unhover();
+			active_cell = ray.GameObject;
+			active_cell.Components.Get<Cell>().Hover();
+		}
+		else if(ray.Hit && ray.GameObject.Tags.Has("back")){
+			if(active_cell == null) return;
+			active_cell.Components.Get<Cell>().Unhover();
+			active_cell = null;
+		}
+	}
+
+	public void SelectCells(){
+		if(Input.Down("interact")){
+			if(active_cell != null){
+				active_cell.Components.Get<Cell>().Select();
+				if(selected_cell != null) selected_cell.Components.Get<Cell>().Unselect();
+				selected_cell = active_cell;
+				selected_cell.Components.Get<Cell>().Select();
+			}
+			else{
+				if(selected_cell != null){
+					selected_cell.Components.Get<Cell>().Unselect();
+					selected_cell = null;
+				}
+			}
+		}
 	}
 }
--- code/Cell.cs
+++ code/Cell.cs
@@ -1,25 +1,72 @@
 using Sandbox;
 
-public sealed class Cell : Component, Component.ITriggerListener
+public sealed class Cell : Component
 {
 	[Property] int row_id = -1;
 	[Property] int column_id = -1;
+
+	ModelRenderer model;
+	Color base_color;
+	bool hovered = false;
+	bool selected = false;
+
+	protected override void OnStart()
+	{
+		model = GameObject.Components.Get<ModelRenderer>();
+		base_color = model.Tint;
+	}
 	protected override void OnUpdate()
 	{
 
 	}
 
-	public void setID(int row, int column){
+	public void SetID(int row, int column){
 		row_id = row;
 		column_id = column;
 	}
 
-	void ITriggerListener.OnTriggerEnter(Collider other){
-		Log.Info("we are hit");
+	public void Hover(){
+		if(!selected){
+			hovered = true;
+			SetCellGreen();
+		}
+		
 	}
 
-	void ITriggerListener.OnTriggerExit(Collider other) {
-		Log.Info("we are no longer hit");
+	public void Unhover(){
+		if(!selected){
+			hovered = false;
+			SetCellBaseColor();
+		}
+	}
+
+	public void Select(){
+		selected = true;
+		SetCellBlue();
+	}
+
+	public void Unselect(){
+		selected = false;
+		SetCellBaseColor();
+	}
+
+
+	public void SetCellBlue(){
+		Color blue = new Color();
+		blue.b = 1;
+		blue.a = 1;
+		model.Tint = blue;
+	}
+
+	public void SetCellBaseColor(){
+		model.Tint = base_color;
+	}
+
+	public void SetCellGreen(){
+		Color green = new Color();
+		green.g = 1;
+		green.a = 1;
+		model.Tint = green;
 	}
 
 }
